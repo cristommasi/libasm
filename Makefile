96f2b1e531fa12c1e15b290e192dee3b2fd8a019
vikingokvist <vikingokvist@gmail.com>
2026-06-09 17:59:48 +0200

    start of atoibase

--- Makefile
+++ Makefile
@@ -8,13 +8,17 @@ CFLAGS		= -Wall -Wextra
 RM			= rm -f
 
 
-SRCS    =	src/ft_strlen.s \
-			src/ft_strcpy.s \
-			src/ft_strcmp.s \
-			src/ft_write.s	\
-			src/ft_read.s
+SRCS    	=	src/ft_strlen.s \
+				src/ft_strcpy.s \
+				src/ft_strcmp.s \
+				src/ft_write.s	\
+				src/ft_read.s
 
-OBJS    = $(SRCS:.s=.o)
+SRCS_BONUS	=	bonus/ft_atoi_base_bonus.s
+
+OBJS    	= $(SRCS:.s=.o)
+OBJS_BONUS  = $(SRCS_BONUS:.s=.o)
+
 
 TEST		= test_libasm
 TEST_BONUS	= test_libasm_bonus
@@ -29,7 +33,7 @@ $(NAME): $(OBJS)
 	$(NCC) $(ASMFLAGS) -o $@ $<
 
 clean:
-	rm -f $(OBJS)
+	rm -f $(OBJS) $(BONUS_OBJS)
 
 fclean: clean
 	rm -f $(NAME) $(TEST)
@@ -40,12 +44,13 @@ re: fclean all
 test: $(NAME)
 	$(CC) $(CFLAGS) -o $(TEST) main.c -L. -lasm
 
-# bonus: $(OBJS) $(BONUS_OBJS)
-# 	ar rcs $(NAME) $(OBJS) $(BONUS_OBJS)
 
-# test_bonus: bonus
-# 	gcc $(CFLAGS) -L. -lasm -o $(TEST_BONUS) main_bonus.c
-# 	./$(TEST_BONUS)
+bonus: $(OBJS) $(BONUS_OBJS)
+	ar rcs $(NAME) $(OBJS) $(BONUS_OBJS)
+
+test_bonus: bonus
+	gcc $(CFLAGS) -L. -lasm -o $(TEST_BONUS) main_bonus.c
+
 
 
 .PHONY: clean fclean re
--- libft_examples/ft_atoi_base.c
+++ libft_examples/ft_atoi_base.c
@@ -1,23 +1,90 @@
+#include <stdio.h>
+char base_symbols[93] = {-1};
 
-int	_ft_atoi(const char *str)
+int		is_sign(char c) {
+
+	return (c == '+' || c == '-');
+}
+
+int		is_whitespace(char c) {
+
+	return (c == '\t' || c == '\r' || c == '\n' || c == ' ' || c == '\f' || c == '\v');
+}
+
+
+int		in_symbols(char c) {
+
+	return (base_symbols[(unsigned char)c] != -1);
+}
+
+int		check_base(char *base) {
+
+	int i = 0;
+	printf("hello\n");
+	while (base[i]) {
+
+		if (is_sign(base[i])) {
+			printf("is_sign = %c\n, ", base[i]);
+			return (0);
+		}
+		if (is_whitespace(base[i])) {
+			printf("is_whitespace = %c\n", base[i]);
+			return (0);
+		}
+		if (in_symbols(base[i])) {
+			printf("is_whitespace = %c\n", base[i]);
+			return (0);
+		}
+		base_symbols[(unsigned char)base[i]] = i;
+		i++;
+	}
+	return (i);
+}
+
+
+int		ft_atoi_base(char *str, char *base)
 {
-	int	res;
-	int	minus;
-	int	i;
+	
+	int n_base = check_base(base);
+	if (n_base <= 1)
+		return (0);
 
-	i = 0;
-	res = 0;
-	minus = 1;
-	while (str[i] == 32 || (*str >= 9 && *str <= 13))
+	while (is_whitespace(*str))
 		str++;
+
+	int sign = 1;
 	if (*str == '-')
-		minus *= -1;
-	if (*str == '-' || *str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
 	{
-		res = res * 10 + *str - '0';
+		sign = -1;
 		str++;
 	}
-	return (res * minus);
+	else if (*str == '+') {
+		sign = 1;
+		str++;
+	}
+
+    int result = 0;
+	while (*str != 0)
+	{
+		if (!in_symbols(*str))
+			return (0);
+		result = result * n_base + base_symbols[*str];
+		str++;
+	}
+	return (result * sign);
+}
+
+
+
+int main(void) {
+
+
+
+	printf("num = %d \n", ft_atoi_base("42", "0123456789"));
+
+	
+	
+	
+	
+	return 0;
 }
